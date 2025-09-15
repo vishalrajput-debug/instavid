6b007cb445c80028c885b214495d32f636a8bad5
vishalrajput-debug <vishal.rajput@we24seven.in>
2025-09-15 23:52:09 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on first error
-set -o errexit
-
-# Install ffmpeg
-apt-get update && apt-get install -y ffmpeg
-
-# Install Python dependencies
-pip install -r requirements.txt
